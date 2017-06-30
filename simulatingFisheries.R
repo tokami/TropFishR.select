b8a21928d1f3a2793d41fce74527d67ac92657c0
tokami <tobi_mildenberger@yahoo.de>
2017-06-30 23:03:07 +0200

    starting descriptive document

--- simulatingFisheries.R
+++ simulatingFisheries.R
@@ -5,6 +5,8 @@
 #' export: html_document
 #'---
 #'
+#' Load packages
+require(TropFishR)
 require(TropFishR.select)
 #'
 #' ## Introduction to stockSim.R
@@ -12,7 +14,7 @@ Linf.mu = 80
 L50 = 0.25*Linf.mu
 wqs = L50*0.2
 ex1 <- stockSim(Linf.mu = Linf.mu, L50 = L50,
-                wqs = wqs)
+                wqs = wqs, progressBar = FALSE)
 
 #' Fishing mortality
 plot(as.Date(ex1$fisheries$fished_years, "%Y-%m-%d"), ex1$fisheries$F, 
@@ -27,7 +29,6 @@ plot(Lt,pt1,type="l", col = 'blue',lwd=2,
      xlab = "Length", ylab = "Probability of capture")
 
 #' Resulting LFQ data
-require(TropFishR)
 class(ex1$lfqbin) <- "lfq"
 plot(ex1$lfqbin, Fname = "catch")
 
